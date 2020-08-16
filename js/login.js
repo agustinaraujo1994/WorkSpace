3102c35c34147a5ba4e911a507439a4e2e67a613
agustinaraujo1994 <haar>
2020-08-16 11:56:21 -0300

    Cargar

--- js/login.js
+++ js/login.js
@@ -2,5 +2,8 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    sessionStorage.Apellido = 'Márquez Montoya';
+    let mostrar = sessionStorage.apellido;
+    console.log(mostrar);
 
 });
